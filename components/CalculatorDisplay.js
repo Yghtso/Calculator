053c2a180d465fb2e827c897a3ab99cb7860306d
Cristian Soare <soare.cristian@outlook.com>
2025-03-30 12:11:23 +0200

    Aggiunto anche lo stile base del display

---
+++ components/CalculatorDisplay.js
@@ -0,0 +1,30 @@
+import { Text, StyleSheet, View, SafeAreaView } from "react-native";
+
+export default CalculatorDisplay = () => {
+    return (
+        <View style={styles.displayContainer}>
+            <Text style={styles.resultText}>RISULTATO</Text>
+            <Text style={styles.expressionText}>EXPR</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    displayContainer: {
+        height: "40%",
+        backgroundColor: '#000000',
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        padding: 20,
+    },
+    resultText: {
+        fontSize: 48,
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    expressionText: {
+        fontSize: 24,
+        color: '#aaa',
+        marginBottom: 10,
+    },
+});
