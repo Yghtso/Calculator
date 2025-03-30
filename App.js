62df5958a68fa222dd52d643cb75e425fd460475
Cristian Soare <soare.cristian@outlook.com>
2025-03-30 12:12:50 +0200

    Modificata leggermente la struttura di App.js

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import CalculatorKeyboard from './components/CalculatorKeyboard';
+import CalculatorDisplay from './components/CalculatorDisplay';
+import { StatusBar } from 'react-native';
 
-export default function App() {
+export default App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="black" />
+      <CalculatorDisplay />
+      <CalculatorKeyboard />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
